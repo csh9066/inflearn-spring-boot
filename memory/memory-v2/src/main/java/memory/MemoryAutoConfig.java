6b567236a79e651cfae8efb9462ae4532f3724e6
seunglo <csh9066@gmail.com>
2023-09-18 22:44:20 +0900

    autoconfig - 자동 구성 라이브러리 만들기 실습 완료

---
+++ memory/memory-v2/src/main/java/memory/MemoryAutoConfig.java
@@ -0,0 +1,20 @@
+package memory;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+@ConditionalOnProperty(name = "memory", havingValue = "on")
+@AutoConfiguration
+public class MemoryAutoConfig {
+
+    @Bean
+    public MemoryController memoryController() {
+        return new MemoryController(memoryFinder());
+    }
+
+    @Bean
+    public MemoryFinder memoryFinder() {
+        return new MemoryFinder();
+    }
+}
--- memory/project-v2/src/main/java/hello/ProjectV2Application.java
+++ memory/project-v2/src/main/java/hello/ProjectV2Application.java
@@ -1,7 +1,9 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ProjectV2Application {
@@ -9,5 +11,4 @@ public class ProjectV2Application {
     public static void main(String[] args) {
         SpringApplication.run(ProjectV2Application.class, args);
     }
-
 }
